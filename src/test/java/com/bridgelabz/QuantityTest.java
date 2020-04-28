1f3c944a7c7d109bb79dc91bbf1739c82e2cb23d
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-04-29 01:26:30 +0530

    [BHUSHAN]ADDED:UC1-Added code to test the equality of 2 feets.

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -1,4 +1,13 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class QuantityTest {
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnEquals() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1,feet2);
+    }
 }
