4eb382fd276382bebbda75132c6cf27e00ec490b
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-04-29 01:58:11 +0530

    [BHUSHAN]REFACTERED:UC1-Resolved code Feet and Inch Combined Convert Length.

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -6,21 +6,34 @@ import org.junit.Test;
 public class QuantityTest {
     @Test
     public void given0FeetAnd0Feet_ShouldReturnEquals() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,0.0);
         Assert.assertEquals(feet1,feet2);
     }
     @Test
     public void given0FeetAnd1Feet_ShouldReturnNotEquals() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
         Assert.assertNotEquals(feet1,feet2);
     }
 
     @Test
     public void given0InchAnd0Inch_ShouldReturnEqual() {
-        Inch inch1 = new Inch(0.0);
-        Inch inch2 = new Inch(0.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inch1, inch2);
+    }
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
+        Assert.assertNotEquals(inch1, inch2);
+    }
+    @Test
+    public void given1FeetAnd1Inch_ShouldReturnNotEquals() {
+        Length feet = new Length(Length.Unit.FEET,1.0);
+        Length inch = new Length(Length.Unit.INCH, 1.0);
+        Assert.assertNotEquals(feet,inch);
+
     }
 }
